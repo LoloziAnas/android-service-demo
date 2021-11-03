bcc64208fcc8eee679ea3861249121ec89640664
LoloziAnas <lolozianas1@gmail.com>
2021-11-03 20:11:50 +0000

    Implementing MainActivity & NextActivity

---
+++ app/src/main/java/com/lzitech/servicesdemoapp/NextActivity.java
@@ -0,0 +1,14 @@
+package com.lzitech.servicesdemoapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class NextActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_next);
+    }
+}
