9ab360bf0a30f16da7ab57f7079226e1d5c2c143
LoloziAnas <lolozianas1@gmail.com>
2021-11-03 21:04:58 +0000

    Finishing Implemeting MainActivity & MyService

--- app/src/main/java/com/lzitech/servicesdemoapp/MainActivity.java
+++ app/src/main/java/com/lzitech/servicesdemoapp/MainActivity.java
@@ -1,14 +1,39 @@
 package com.lzitech.servicesdemoapp;
 
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.Button;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
-
 public class MainActivity extends AppCompatActivity {
+
+    private Button buttonStart, buttonStop, buttonNext;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        // binding method
+        bind();
+        buttonStart.setOnClickListener(v -> {
+            startService(new Intent(this, MyService.class));
+        });
+        buttonStop.setOnClickListener(v -> {
+            stopService(new Intent(this, MyService.class));
+        });
+        buttonNext.setOnClickListener(v -> {
+            Intent intent = new Intent(this, NextActivity.class);
+            startActivity(intent);
+        });
+
     }
+
+    private void bind() {
+        buttonStart = findViewById(R.id.buttonStart);
+        buttonStop = findViewById(R.id.buttonStop);
+        buttonNext = findViewById(R.id.buttonNext);
+    }
+
+
 }
--- app/src/main/java/com/lzitech/servicesdemoapp/MyService.java
+++ app/src/main/java/com/lzitech/servicesdemoapp/MyService.java
@@ -4,6 +4,7 @@ import android.app.Service;
 import android.content.Intent;
 import android.media.MediaPlayer;
 import android.os.IBinder;
+import android.widget.Toast;
 
 import androidx.annotation.Nullable;
 
@@ -12,17 +13,29 @@ public class MyService extends Service {
     @Nullable
     @Override
     public IBinder onBind(Intent intent) {
+        // We don't provide binding, so return null
         return null;
     }
 
     @Override
     public void onCreate() {
+        Toast.makeText(this, "Service Saved ..", Toast.LENGTH_LONG).show();
+        mediaPlayer = MediaPlayer.create(this, R.raw.sun);
+        mediaPlayer.setLooping(false);
         super.onCreate();
     }
 
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+        Toast.makeText(this, "Service Starting ..", Toast.LENGTH_SHORT).show();
+        mediaPlayer.start();
+        // if we get killed, after returning from here, restart.
+        return START_STICKY;
+    }
 
     @Override
     public void onDestroy() {
-        super.onDestroy();
+        Toast.makeText(this, "Service Done ..", Toast.LENGTH_SHORT).show();
+        mediaPlayer.stop();
     }
 }
