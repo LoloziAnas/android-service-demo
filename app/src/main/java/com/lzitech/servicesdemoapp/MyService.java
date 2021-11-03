df26ff7ca8d1dae8374c235d17daf6d70ba9ee5c
LoloziAnas <lolozianas1@gmail.com>
2021-11-03 20:36:15 +0000

    Implemeting Service class

---
+++ app/src/main/java/com/lzitech/servicesdemoapp/MyService.java
@@ -0,0 +1,28 @@
+package com.lzitech.servicesdemoapp;
+
+import android.app.Service;
+import android.content.Intent;
+import android.media.MediaPlayer;
+import android.os.IBinder;
+
+import androidx.annotation.Nullable;
+
+public class MyService extends Service {
+    private MediaPlayer mediaPlayer;
+    @Nullable
+    @Override
+    public IBinder onBind(Intent intent) {
+        return null;
+    }
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+    }
+}
